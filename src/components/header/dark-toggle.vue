67c474d12eea499695fb7fa2826160d67760ed43
sxy15 <sxy_19921115@163.com>
2024-07-17 15:57:57 +0800

    feat: toggle icon

--- src/components/header/dark-toggle.vue
+++ src/components/header/dark-toggle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="!outline-none cursor-pointer w-[40px] text-center" @click="toggleDark">
-    <Icon v-if="isDark" class="h-[16px] w-[16px] " icon="carbon-moon" />
-    <Icon v-else class="h-[16px] w-[16px] " icon="carbon-sun" />
+    <Icon v-if="isDark" class="h-[16px] w-[16px] text-white" icon="carbon-moon" />
+    <Icon v-else class="h-[16px] w-[16px]" icon="carbon-sun" />
   </div>
 </template>
 
