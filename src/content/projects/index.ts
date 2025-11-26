c68e8232339ef99ffe5ec6a4e24b62b7da251740
sxy15 <sxy_19921115@163.com>
2025-11-26 09:54:39 +0800

    feat: add font-subset project

--- src/content/projects/index.ts
+++ src/content/projects/index.ts
@@ -15,5 +15,13 @@ export const projects: Project[] = [
     image: 'https://images.unsplash.com/photo-1436491865332-7a61a109cc05?auto=format&fit=crop&w=800&q=80',
     url: 'https://github.com/sxy15/github-image-upload',
     tags: ['vscode', 'extension']
+  },
+  {
+    id: '2',
+    title: 'font subset',
+    description: 'create a font-subset from a font file.',
+    image: 'https://raw.githubusercontent.com/sxy15/bbb/main/assets/a7c41232-f50c-56ad-37c6-7593a25c18bc.png',
+    url: 'https://github.com/sxy15/font-subset',
+    tags: ['font', 'subset']
   }
 ];
